276ccbe0c6b4212642c2e5739280290cc56c8ad1
ST10434179-MichaelHerring <ST10434179@vcconnect.edu.za>
2025-12-12 08:40:55 +0200

    added interface "IOperations"

---
+++ src/Question1/IOperation.java
@@ -0,0 +1,17 @@
+package Question1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author micha
+ */
+public interface IOperation {
+    int TotalOperations(int[][] operations);
+    double AverageOperations(int[][] operations);
+    int MaxOperations(int[][] operations);
+    int MinOperations(int[][] operations);
+}
